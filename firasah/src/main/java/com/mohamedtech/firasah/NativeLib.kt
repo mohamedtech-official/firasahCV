3fd09df64df53a3f4711137f0a7da87f8c1e34ec
Mohamed Goush <69215006+MohamedGoush@users.noreply.github.com>
2024-01-10 15:20:36 +0530

    FirasahCV init Commit

---
+++ firasah/src/main/java/com/mohamedtech/firasah/NativeLib.kt
@@ -0,0 +1,17 @@
+package com.mohamedtech.firasah
+
+class NativeLib {
+
+    /**
+     * A native method that is implemented by the 'firasah' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'firasah' library on application startup.
+        init {
+            System.loadLibrary("firasah")
+        }
+    }
+}
